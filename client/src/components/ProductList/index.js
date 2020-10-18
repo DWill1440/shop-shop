33d28b15331b59066b22f563cb06de964b5b5646
DWill1440 <dwill2think@gmail.com>
2020-10-17 21:22:59 -0500

    indexDB

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -7,30 +7,51 @@ import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif";
 
+import { idbPromise } from "../../utils/helpers";
+
 function ProductList() {
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
-  
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
+
   useEffect(() => {
     if (data) {
+      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
       });
-    }
-  }, [data, dispatch]);
+
+      // but let's also take each product and save it to IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise("products", "put", product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+  } else if (!loading) {
+    // since we're offline, get all of the data from the `products` store
+    idbPromise('products', 'get').then((products) => {
+      // use retrieved data to set global state for offline browsing
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: products
+      });
+    });
+  }
   
+  }, [data, loading, dispatch]);
+
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
-  
-    return state.products.filter(product => product.category._id === currentCategory);
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
-  
+
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -13,6 +13,7 @@ import {
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from "../assets/spinner.gif";
 import Cart from "../components/Cart";
+import { idbPromise } from "../utils/helpers";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -33,11 +34,18 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise("cart", "put", {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 },
       });
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise("cart", "put", { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -46,6 +54,9 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id,
     });
+
+      // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+  idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   useEffect(() => {
@@ -56,8 +67,21 @@ function Detail() {
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+
+      data.products.forEach((product) => {
+        idbPromise("products", "put", product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise("products", "get").then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts,
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
@@ -71,9 +95,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{" "}
-            
             <button onClick={addToCart}>Add to cart</button>
-
             <button
               disabled={!cart.find((p) => p._id === currentProduct._id)}
               onClick={removeFromCart}
