3ef47202d93691fcfb3dc2e11bfeea2a52059566
DWill1440 <dwill2think@gmail.com>
2020-10-26 09:11:50 -0500

    successful order

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,15 @@ import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 
 import OrderHistory from "./pages/OrderHistory";
+import Success from "./pages/Success";
 
 const client = new ApolloClient({
   request: (operation) => {
     const token = localStorage.getItem("id_token");
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
+        authorization: token ? `Bearer ${token}` : ""
+      }
     });
   },
   uri: "/graphql",
@@ -36,6 +37,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
